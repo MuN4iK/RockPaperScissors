bbfd775f69a9151d4931f65ab2c7000787fd74d7
Данило Гула <danilo3gula@gmail.com>
2026-08-23 20:46:01 +0300

    rework PORT

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express');
 
 const app = express()
-const PORT = 3002
+const PORT = process.env.PORT || 3001;
 
 const jwt = require("jsonwebtoken");
 
